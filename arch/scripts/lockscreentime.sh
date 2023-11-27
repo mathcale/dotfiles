25e015dc73aed037b4fdc3e15a13b04adcf2fd91
Matheus Calegaro <hello@matheus.me>
2023-11-26 23:00:11 -0300

    feat(arch/swaylock): lock screen before sleep

--- arch/scripts/lockscreentime.sh
+++ arch/scripts/lockscreentime.sh
@@ -5,7 +5,11 @@ timeoff=660
 
 if [ -f "/usr/bin/swayidle" ]; then
   echo "swayidle is installed."
-  swayidle -w timeout $timeswaylock 'swaylock -f' timeout $timeoff 'hyprctl dispatch dpms off' resume 'hyprctl dispatch dpms on'
+  swayidle -w \
+    timeout $timeswaylock 'swaylock -f' \
+    timeout $timeoff 'hyprctl dispatch dpms off' \
+    resume 'hyprctl dispatch dpms on' \
+    before-sleep 'swaylock -f -c 000000'
 else
   echo "swayidle not installed."
 fi
