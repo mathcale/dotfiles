dcea635e4b266682fcfa588de03e16c158b3f5bb
Matheus Calegaro <hello@matheus.me>
2026-05-17 18:37:38 -0300

    feat(arch/scripts): add --devel flag to up for opt-in VCS package rebuilds

--- arch/scripts/up.sh
+++ arch/scripts/up.sh
@@ -2,6 +2,15 @@
 
 set -x
 
+DEVEL_FLAG=""
+for arg in "$@"; do
+  case "$arg" in
+    --devel|-d)
+      DEVEL_FLAG="--devel"
+      ;;
+  esac
+done
+
 sudo pacman --noconfirm -Syu
-yay --noconfirm -Syu
+yay --noconfirm -Syu $DEVEL_FLAG
 flatpak upgrade -y
